04fdd798cdf2b7c8fddafb6b14c1def1bfac5e7a
Hugo O. Rivera <hugo@roguh.com>
2022-07-14 23:54:01 -0400

    Refactor main.rs for clarity, add more comments.

--- src/main.rs
+++ src/main.rs
@@ -4,63 +4,78 @@
 use std::io;
 use std::sync::{Arc, Mutex};
 
-use rmodbus::{
-    server::{context::ModbusContext, ModbusFrame},
-    ModbusFrameBuf, ModbusProto,
-};
+use rmodbus::server::{context::ModbusContext, ModbusFrame};
+use rmodbus::{ModbusFrameBuf, ModbusProto};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
-async fn process_socket(
-    unit: u8,
+// Modbus server configuration
+const SERVER_UNIT: u8 = 1;
+const ADDR: &str = "0.0.0.0:5502";
+
+fn print_modbus_context(
+    modbus_context: &Arc<std::sync::Mutex<ModbusContext>>,
+    start_addr: u16,
+    count: u16,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut result: Vec<u16> = vec![];
+    modbus_context
+        .lock()
+        .unwrap()
+        .get_holdings_bulk(start_addr, count, &mut result)?;
+    println!(
+        "16-bit registers from address {} to {}: {:?}",
+        start_addr,
+        start_addr + count,
+        result
+    );
+    Ok(())
+}
+
+async fn handle_connection(
     modbus_context: Arc<std::sync::Mutex<ModbusContext>>,
     stream: &mut TcpStream,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let ip = stream.peer_addr()?;
-    println!("Client connected: {}", &ip);
+    println!("New TCP connection from {}", &ip);
+    // Repeatedly read modbus frames from this TCP connection
     loop {
         let mut buf: ModbusFrameBuf = [0; 256];
         let mut response = Vec::new();
+        // Read from socket until there's no more data immediately available
         if stream.read(&mut buf).await.unwrap_or(0) == 0 {
             break;
         }
-        let mut frame = ModbusFrame::new(unit, &buf, ModbusProto::TcpUdp, &mut response);
-        let parsing_result = frame.parse();
-        if parsing_result.is_err() {
-            println!(
-                "Frame parsing error {:?}. Buffer={:?}",
-                parsing_result, &buf
-            );
-            return parsing_result.map_err(|err_kind| format!("{:?}", err_kind).into());
+
+        // Parse bytes into a modbus frame
+        let mut frame = ModbusFrame::new(SERVER_UNIT, &buf, ModbusProto::TcpUdp, &mut response);
+        if let Err(err) = frame.parse() {
+            println!("Modbus frame parsing error {}. Buffer={:?}", err, &buf);
+            // Convert an rmodbus error into this function's error type
+            return Err(err.into());
         }
+
+        // Modify modbus context if necessary
         if frame.processing_required {
             let result = match frame.readonly {
                 true => frame.process_read(&modbus_context.lock().unwrap()),
                 false => frame.process_write(&mut modbus_context.lock().unwrap()),
             };
-            if result.is_err() {
-                println!("Frame processing error {:?}", result);
-                return result.map_err(|err_kind| format!("{:?}", err_kind).into());
+            if let Err(err) = result {
+                println!("Modbus frame processing error {}", err);
+                return Err(err.into());
             }
         }
 
-        let mut result: Vec<u16> = vec![];
-        modbus_context
-            .lock()
-            .unwrap()
-            .get_holdings_bulk(0, 10, &mut result)?;
-        println!("16-bit registers from address 0 to 9: {:?}", result);
+        print_modbus_context(&modbus_context, 0, 16)?;
 
+        // Send response to client if necessary
         if frame.response_required {
             frame.finalize_response()?;
-            println!(
-                "Responding to client {} with: {:x?}",
-                &ip,
-                response.as_slice()
-            );
-            let stream_write_response = stream.write(response.as_slice()).await;
-            if stream_write_response.is_err() {
-                return stream_write_response.map(|_| ()).map_err(|err| err.into());
+            println!("Responding to {}: {:?}", &ip, response);
+            if let Err(err) = stream.write(response.as_slice()).await {
+                println!("Sending response to client failed: {}", err);
+                return Err(err.into());
             }
         }
     }
@@ -69,19 +84,17 @@ async fn process_socket(
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let addr = "0.0.0.0:5502";
-    let mut listener = TcpListener::bind(addr).await?;
+    let listener = TcpListener::bind(ADDR).await?;
     // The Arc and Mutex allow sharing the context between tokio's async green threads
     let modbus_context = Arc::new(Mutex::new(ModbusContext::new()));
 
-    println!("Listening on {}", addr);
+    println!("Modbus server unit={} listening on {}", SERVER_UNIT, ADDR);
     loop {
         let (mut socket, _) = listener.accept().await?;
-        let modbus_context_wrapper_copy = modbus_context.clone();
+        let modbus_context_arc_copy = modbus_context.clone();
         tokio::spawn(async move {
-            let result = process_socket(1, modbus_context_wrapper_copy, &mut socket).await;
-            if result.is_err() {
-                println!("Error: {:?}", result);
+            if let Err(err) = handle_connection(modbus_context_arc_copy, &mut socket).await {
+                println!("Error: {}", err);
             }
         });
     }
